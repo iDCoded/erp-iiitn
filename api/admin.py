8000030780207589b1dc6b8d0d431db1a5601f7e
Sambodhi <sambodhibhowal@Gmail.com>
2025-02-17 18:43:40 +0530

    intitial database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Payments, Transactions
+
+admin.site.register(Payments)
+admin.site.register( Transactions)
 
 # Register your models here.
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Payments(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    dateDue = models.DateField()
+
+
+class Transactions(models.Model):
+    id = models.AutoField(primary_key=True)
+    Payments= models.ForeignKey(Payments, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    no_of_payments = models.IntegerField(default=1)
+    transaction_id = models.TextField()
+    payment_pic = models.ImageField()
+    verified = models.BooleanField()
+    payment_datetime = models.DateTimeField()
+    
+
+
