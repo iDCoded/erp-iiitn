dca7f963c8ec86367a926acee4cfc546d2fa2615
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-18 08:06:59 +0530

    Added user class to serializers.py

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import  serializers
+from django.contrib.auth.models import User
+
+# User serializer class (Inherited from DJANGO Auth)
+# Contains user information fields - ID, username, email, password
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = User
+        fields = ['id', 'username', 'email', 'password']
