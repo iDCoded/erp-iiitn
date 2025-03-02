11d2ba9f0b5e7c53f21389b933325226a876e633
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-03 00:31:14 +0530

    Add route to get transactions & payments

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
-from .views import login, signup, validate_token
+from .views import login, signup, validate_token, PaymentsListView, TransactionsListView
 
 urlpatterns = [
     re_path('login/' , login , name='login'), # /{{base_url}}/api/login/
     re_path('signup/', signup, name='signup'), # /{{base_url}}/api/signup/
-    re_path('token', validate_token, name='validate') # /{{base_url}}/api/token/
+    re_path('token', validate_token, name='validate'),  # /{{base_url}}/api/token/
+    re_path('payments', PaymentsListView.as_view(), name="payments-list"),  # /{{base_url}}/api/payments/
+    re_path('transactions', TransactionsListView.as_view(), name="transactions-list")  # /{{base_url}}/api/transactions/
 ]
